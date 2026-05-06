b6eb7786cccbed4df5382f6041320cfcede1c754
문지호 <moonjiho0706@yonsei.ac.kr>
2026-05-06 14:22:54 +0900

    [FEAT] 쇼핑몰 커스터마이징

--- src/pages/Products.tsx
+++ src/pages/Products.tsx
@@ -4,9 +4,16 @@ import { useState } from "react";
 
 export default function Products() {
   const [search, setSearch] = useState("");
-  const filteredProducts = products.filter((product) =>
-    product.name.toLowerCase().includes(search.toLowerCase())
-  );
+  const [category, setCategory] = useState("전체");
+
+  const categories = ["전체", "상의", "아우터", "하의", "신발", "가방", "소품"];
+
+  const filteredProducts = products.filter((product) => {
+    const Search = product.name.toLowerCase().includes(search.toLowerCase());
+    const Category = category === "전체" || product.category === category;
+
+    return Search && Category;
+  });
   return (
     <div>
       <input
@@ -16,6 +23,22 @@ export default function Products() {
         onChange={(e) => setSearch(e.target.value)}
         className="w-full mb-6 px-4 py-2 border border-gray-300 rounded-lg focus:outline-none focus:ring-2 focus:ring-blue-500"
       />
+
+      <div className="flex gap-2 mb-6">
+        {categories.map((item) => (
+          <button
+            key={item}
+            onClick={() => setCategory(item)}
+            className={`px-4 py-2 rounded-lg border transition 
+              ${category === item
+                ? "bg-black text-white"
+                : "bg-white hover:bg-gray-100"
+              }`}
+          >
+            {item}
+          </button>
+        ))}
+      </div>
 
       <h2 className="text-2xl font-bold mb-6">상품 목록</h2>
 
